921c488a6117a43855d2cf223dfc11bfce453884
Omar-coder-ai <geekandgraphics@outlook.com>
2022-01-21 16:11:02 +0100

    Modifica in app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ import Header from "./components/Header/Header";
 import Topbar from "./components/Topbar/Topbar";
 import Home from "./pages/home/Home";
 import Footer from "./components/Footer/Footer";
+import Attualitaepolitica from "./pages/Attualitaepolitica";
+import Internazionale from "./pages/Internazionale";
+import Societa from "./pages/Societa";
+import Ambiente from "./pages/Ambiente";
+import Profili from "./pages/Profili";
 
 function App() {
   return (
@@ -17,6 +22,7 @@ function App() {
         <Route component={Ambiente} path="/ambiente" />
         <Route component={Profili} path="/profili" />
       </Switch>
+      <Footer />
     </Router>
   );
 }
